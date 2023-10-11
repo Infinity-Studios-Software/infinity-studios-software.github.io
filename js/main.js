874ba10e0691dc3fbe1314ef37d82b846d5ea512
Infinity-Studios-Software <145294987+Infinity-Studios-Software@users.noreply.github.com>
2023-10-11 20:26:06 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 //Import the THREE.js library
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-// To allow for the camera to move around the scene
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-// To allow for importing the .gltf file
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 import { MyAxis } from './MyAxis.js';
+
 //Create a Three.JS Scene
 const scene = new THREE.Scene();
 //create a new camera with positions and angles
 let container = document.getElementById("container3D");
-const camera = new THREE.PerspectiveCamera(80, container.clientWidth / container.clientHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(64, container.clientWidth / container.clientHeight, 0.1, 1000);
 
 //Keep track of the mouse position, so we can make the eye move
 let mouseX = window.innerWidth / 2;
@@ -33,8 +32,8 @@ loader.load(
   function (gltf) {
     //If the file is loaded, add it to the scene
     object = gltf.scene;
-    object.rotation.set(0, 0, 0);
-    //object.scale.set(1.5, 0.2, 1.5);
+    object.rotation.set(-Math.PI/50, Math.PI*1.25, Math.PI/15);
+    object.scale.set(10, 10, 10);
     object.position.set(0, 0.3, 0.2);
     scene.add(object);
   },
@@ -49,7 +48,7 @@ loader.load(
 );
 
 
-camera.position.set(-1.4, 0.7, 1.3); // Adjust the camera position
+camera.position.set(-1.4, 0.5, 1.3); // Adjust the camera position
 camera.lookAt(0, 0, 0); // Look at the origin (0, 0, 0)
 
 
@@ -59,20 +58,23 @@ renderer.setPixelRatio(window.devicePixelRatio); // This line is important!
 container = document.getElementById("container3D");
 renderer.setSize(container.clientWidth, container.clientHeight);
 
+
 //Add the renderer to the DOM
 document.getElementById("container3D").appendChild(renderer.domElement);
 
 
 //Add lights to the scene, so we can actually see the 3D model
-const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
-topLight.position.set(2, 2, 2) //top-left-ish
+const topLight = new THREE.DirectionalLight(0xffffff, 0.63); // (color, intensity)
+topLight.position.set(-4.5, 4, 3) //top-left-ish
 scene.add(topLight);
 
-const ambientLight = new THREE.AmbientLight(0x333333, 2);
-scene.add(ambientLight);
+//Add lights to the scene, so we can actually see the 3D model
+const bottomLight = new THREE.DirectionalLight(0xffffff, 0.49); // (color, intensity)
+bottomLight.position.set(0, 2, 10) //bottom left ish
+scene.add(bottomLight);
 
-//let axis = new MyAxis();
-//scene.add(axis);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
 // Set up target rotation variables
 let targetRotationX = 0;
@@ -81,31 +83,30 @@ let targetRotationY = -0.5;
 // Define a lerp factor. Values closer to 0 will make the rotation adjust more slowly.
 const lerpFactor = 0.02;
 
-// Variables for oscillation
 let time = 0;
-const amplitudeX = 0.02;  // Maximum angle of oscillation in radians for X-axis
-const frequencyX = 0.5;  // Speed of oscillation for X-axis
+const amplitudeX = 0.01;  // Maximum angle of oscillation in radians for X-axis
+const frequencyX = 1;  // Speed of oscillation for X-axis
 
-const amplitudeY = 0.04;  // Maximum angle of oscillation in radians for Y-axis
-const frequencyY = 0.6;  // Speed of oscillation for Y-axis
+const amplitudeY = 0.06;  // Maximum angle of oscillation in radians for Y-axis
+const frequencyY = 0.7;  // Speed of oscillation for Y-axis
 
 function animate() {
   requestAnimationFrame(animate);
 
-  // Oscillate the targetRotationX and targetRotationY values
-  targetRotationX = amplitudeX * Math.sin(time * frequencyX);
-  targetRotationY = -0.5 + amplitudeY * Math.sin(time * frequencyY); // Added oscillation to the Y rotation
+  if(object != null) {
+  // // Oscillate the targetRotationX and targetRotationY values
+  targetRotationX = -Math.PI/60 + amplitudeX * Math.sin(time * frequencyX);
+  targetRotationY = Math.PI*1.23 + amplitudeY * Math.sin(time * frequencyY); // Added oscillation to the Y rotation
   
-  if (objToRender === "phone") {
-    // Adjust target rotation value for Y based on mouse position
-    targetRotationY += mouseX * 1 / (window.innerWidth * 2);
+  // Adjust target rotation value for Y based on mouse position
+  targetRotationY += mouseX * 1 / (window.innerWidth * 3.2);
 
-    // Use lerp to smoothly adjust the rotation
-    object.rotation.y += (targetRotationY - object.rotation.y) * lerpFactor;
-    object.rotation.x += (targetRotationX - object.rotation.x) * lerpFactor;
-  }
+  // Use lerp to smoothly adjust the rotation
+  object.rotation.y += (targetRotationY - object.rotation.y) * lerpFactor;
+  object.rotation.x += (targetRotationX - object.rotation.x) * lerpFactor;
 
-  time += 0.01; // Increment time
+  time += 0.01; 
+}
 
   renderer.render(scene, camera);
 }
